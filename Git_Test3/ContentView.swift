0514a205e9cb58c3f8df04951a006e93304bfe23
Bright-jeans <Bright.jjeans@gmail.com>
2024-04-18 20:48:21 +0900

    Initial Commit

---
+++ Git_Test3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Git_Test3
+//
+//  Created by Leo Yoon on 4/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Git_Test3/Git_Test3App.swift
@@ -0,0 +1,17 @@
+//
+//  Git_Test3App.swift
+//  Git_Test3
+//
+//  Created by Leo Yoon on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Git_Test3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
