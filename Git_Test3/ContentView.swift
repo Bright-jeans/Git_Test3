498dfd7a5047441df6fe67857d6ab14ebe18f090
Bright-jeans <Bright.jjeans@gmail.com>
2024-04-18 20:52:11 +0900

    2nd commit

--- Git_Test3/ContentView.swift
+++ Git_Test3/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, Leo!")
+            Text("Hello, Leo!")
+            Text("Hello, Leo!")
         }
         .padding()
     }
